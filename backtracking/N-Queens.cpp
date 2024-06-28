7a60a7720474dd454725a47e8fbac7e4eb59a457
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-06-28 09:26:41 +0530

    Update N-Queens.cpp

--- backtracking/N-Queens.cpp
+++ backtracking/N-Queens.cpp
@@ -42,3 +42,53 @@ class Solution {
       return ans;
     }
 };
+
+//updated code more readable
+class Solution {
+public:
+
+
+
+    void solve(int col,vector<string>&board,vector<int>&rowHash,vector<int>&downDigonal,vector<int>&upDigonal,vector<vector<string>>&res,int&n){
+        if(col==n){
+            res.push_back(board);
+            return;
+        }
+
+        for(int row= 0;row<n;row++){
+            if(rowHash[row]==0&&downDigonal[row+col]==0&&upDigonal[n-1+col-row]==0){
+                board[row][col]='Q';
+                rowHash[row]=1;
+                downDigonal[row+col]=1;
+                upDigonal[n-1+col-row]=1;
+                solve(col+1,board,rowHash,downDigonal,upDigonal,res,n);
+                board[row][col]='.';
+                rowHash[row]=0;
+                downDigonal[row+col]=0;
+                upDigonal[n-1+col-row]=0;                
+
+
+            }
+        }
+
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        //we will fill columns recursively so we need 3 hashes
+        //filled row, down digonal , up digonal
+
+        vector<int>rowHash(n,0);
+        vector<int>downDigonal(2*n-1,0);
+        vector<int>upDigonal(2*n-1,0);
+        vector<vector<string>>res;
+
+        vector<string>board(n,string(n,'.'));
+        solve(0,board,rowHash,downDigonal,upDigonal,res,n);
+        return res;
+        // for(int i=0;i<n;i++){
+        //     cout<<board[i]<<endl;
+        // }
+        // return {{}};
+        
+    }
+};
