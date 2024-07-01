785eb7402f26a7841eedf00c69eced9b35f6395d
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-07-01 14:01:39 +0530

    C++ modulo of negative numbers issue

---
+++ Bit manipulation/Implement Power Function(ib).cpp
@@ -0,0 +1,20 @@
+#define ll long long 
+
+int Solution::pow(int x, int n, int d) {
+if(d==1)return 0; 
+  ll ans = 1;
+  ll base = 1ll*x%d;
+  if(n==0)ans = base%d;
+  while(n>0){
+    if(n&1)ans =(ans*base)%d;
+
+    base = (base*base)%d;
+    n=n>>1;
+  }
+  if (ans>=0) return ans ;
+  else return d +ans;
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+}
