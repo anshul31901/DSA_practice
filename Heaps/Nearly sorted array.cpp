f11fcc04d97027ce95bc2b71b0a72d68961de668
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-07-03 09:24:11 +0530

    Create Nearly sorted array.cpp

---
+++ Heaps/Nearly sorted array.cpp
@@ -0,0 +1,30 @@
+//we know that 0 th element will lie in pos 0 to k , therefore 1st k +1 elements 
+//min of k +1 elements on every step
+//create a min Heap keep pushing elements in it
+// if size>k pop element and include in array 
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        
+        priority_queue<int,vector<int>,greater<int>>minH;
+        vector<int>res;
+        
+        for(int i = 0 ;i<num;i++){
+            minH.push(arr[i]);
+            if(minH.size()>K){
+                res.push_back(minH.top());
+                minH.pop();
+            }
+            
+            
+        }
+        while(!minH.empty()){
+            res.push_back(minH.top());
+            minH.pop();
+        }
+        return res;
+    }
+};
