3e0de9f915a3ac1741b3dbed26a1bd7632fc919c
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-07-12 23:28:59 +0530

    Update 150. Evaluate Reverse Polish Notation.cpp

--- Stack and queue/150. Evaluate Reverse Polish Notation.cpp
+++ Stack and queue/150. Evaluate Reverse Polish Notation.cpp
@@ -25,3 +25,31 @@ public:
         
     }
 };
+
+//here is better complexity solution:
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+
+        stack<string>st;
+
+        for(auto s:tokens){
+            if(s.size()==1&&(s[0]=='+'||s[0]=='/'||s[0]=='-'||s[0]=='*')){
+                int a = stoi(st.top());
+                st.pop();
+                int b = stoi(st.top());
+                st.pop();
+
+                if(s[0]=='+')st.push(to_string(a+b));
+                else if(s[0]=='-')st.push(to_string(b-a));
+                else if(s[0]=='*')st.push(to_string(a*b));
+                else st.push(to_string(b/a));
+            }
+            else st.push(s);
+
+    
+        }
+        return stoi(st.top());
+        
+    }
+};
