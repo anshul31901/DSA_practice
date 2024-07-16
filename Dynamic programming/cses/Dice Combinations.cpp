4fe59db4d242f9f8c5df344bef04df5249e40245
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-07-16 10:56:50 +0530

    Update Dice Combinations.cpp

--- Dynamic programming/cses/Dice Combinations.cpp
+++ Dynamic programming/cses/Dice Combinations.cpp
@@ -25,3 +25,40 @@ int main(){
 
 
 }
+
+//space optimised as we only need at most 6 prevsious dp states
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+
+
+const int MOD = 1e9+7;
+int main(){
+
+
+	int n;
+	cin>>n;
+	//base case : when to make zero we need only one combination to not throw dice
+	vector<int>prev(1,1);//only contains dp[0]
+	for(int i =1;i<=n;i++){
+		int current = 0;
+		for(auto &e:prev){
+			current = (current+e)%MOD;
+		}
+		prev.push_back(current);
+
+		//as i need only 6 prev values everytime
+		if(prev.size()>6){prev.erase(prev.begin());}
+		
+	}
+	cout<<prev.back()<<endl;
+
+
+
+
+
+
+
+
+}
