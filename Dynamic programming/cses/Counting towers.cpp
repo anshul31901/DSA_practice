b0be7893bc351573c13676fec271cd3589eeec67
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2024-07-19 20:56:02 +0530

    time optimisation

--- Dynamic programming/cses/Counting towers.cpp
+++ Dynamic programming/cses/Counting towers.cpp
@@ -36,3 +36,42 @@ int main(){
 
  
 }
+// time optimisation : instead of calculating for n t times seperately.. precompute all upto 1e6 values and just answer the queries
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ 
+ vector<vector<int>>dp(1e6+1,vector<int>(2,0));
+
+ //complexity = O(n)
+ //space complexity : O(1e6*2)
+const int MOD = 1e9+7;
+int main(){
+		dp[1e6][0]=1;
+		dp[1e6][1]=1;
+
+		for(int i = 1e6-1;i>=1;i--){
+
+			dp[i][0]= (1ll*2*dp[i+1][0]+dp[i+1][1])%MOD;
+			dp[i][1]= (1ll*4*dp[i+1][1]+dp[i+1][0])%MOD;
+
+		}
+	int t ;
+	cin>>t;
+
+
+	while(t--){
+		int n ;
+		cin>>n;
+	//we need a n length block , we cans ee this as our base of the tower starts as (1e6-n)th row
+	//so either horizontal block can start from the base or 2 vertical ones so summation of both these cases 
+	// for our next roe is ans 
+
+
+		cout<<(dp[1e6-(n-1)][0]+dp[1e6-(n-1)][1])%MOD<<endl;
+	}
+	
+
+ 
+}
+
