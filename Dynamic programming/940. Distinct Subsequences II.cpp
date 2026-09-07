eb10dacf037aa051d9216f97a42996e283080162
anshul31901 <97424738+anshul31901@users.noreply.github.com>
2026-09-08 00:14:48 +0530

    Implement distinct subsequences calculation

---
+++ Dynamic programming/940. Distinct Subsequences II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        long long MOD = 1e9 + 7;
+        int n = s.size();
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;  
+
+        vector<int> last(26, -1);  
+
+        for (int i = 1; i <= n; i++) {
+            char c = s[i - 1];
+            dp[i] = (2 * dp[i - 1]) % MOD;
+            if (last[c - 'a'] != -1) {
+                dp[i] = (dp[i] - dp[last[c - 'a'] - 1] + MOD) % MOD;
+            }
+            last[c - 'a'] = i;
+        }
+
+        return (dp[n] - 1 + MOD) % MOD;  
+    }
+};
